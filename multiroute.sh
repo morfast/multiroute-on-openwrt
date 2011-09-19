aec8a3fcbd838bdfaf52ebdd30e671def7379b5a
morfast <morfast@dorm.(none)>
2011-09-20 01:01:20 +0800

    adjust output

--- multiroute.sh
+++ multiroute.sh
@@ -12,22 +12,24 @@ echo -1 > /proc/sys/net/ipv4/rt_cache_rebuild_count
 
 echo "testing ${PPP_NUM} connections"
 TIMEOUT=$(( PPP_NUM / 2 + 9))
-echo timeout: ${TIMEOUT}
+echo "timeout: ${TIMEOUT}"
 
 count=0
 while :
 do
     NLINE=$(ifconfig | grep ${PPP_IF_PREFIX} | wc -l)
     if [ ${NLINE} -eq ${PPP_NUM} ]; then
+    	echo
         echo "Congratulations! All ${NLINE} connections established"
         break
     fi  
 
     sleep 1
-    echo '.'
+    echo -n '.'
     count=$(( count + 1 ))
     if [ $count -gt ${TIMEOUT} ]; then
     	if [ ${NLINE} -le 0 ]; then
+            echo
     	    echo "No connection established, trying with less connection number"
     	    pkill pppd
     	    pkill initppp
@@ -36,6 +38,7 @@ do
     	    exit 1
     	fi
         NLINE=$(ifconfig | grep ${PPP_IF_PREFIX} | wc -l)
+	echo
         echo "${NLINE} connections established"
     	pkill initppp
     	break
